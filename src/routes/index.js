c56cd97059343c516a375a9092de19376b1cb78b
melina yanguas <melinayanguas@gmail.com>
2021-02-25 01:18:14 -0300

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+const { Router } = require('express');
+const router = Router();
+const admin = require('firebase-admin');
+
+var serviceAccount = require("../../fir-node-contacts-firebase-adminsdk-pesv9-1928d09330.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://fir-node-contacts-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.database();
+
+router.get('/', (req,res) => {
+    db.ref('contacts').once('value', (snapshot) => {
+        const data = snapshot.val();
+        res.render('index', { contacts: data});
+    });
+    
+});
+
+router.post('/new-contact', (req,res) => {
+    console.log(req.body);
+    const newContact = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone
+    };
+    db.ref('contacts').push(newContact);
+    res.redirect('/');
+});
+
+router.get('/delete-contact/:id', (req,res) => {
+    db.ref('contacts/' + req.params.id).remove();
+    res.redirect('/');
+});
+
+module.exports = router; 
